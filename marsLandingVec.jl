8bd24304ca2fca63f5f99606fb6c686130168ff8
Adrian Fok <adrian.fok22@imperial.ac.uk>
2024-02-29 13:01:54 +0000

    Got IPOPT working but incorrect results
    - Very slow convergence
    - Dynamics do not make sense

--- marsLandingVec.jl
+++ marsLandingVec.jl
@@ -3,6 +3,7 @@ using Pkg
 Pkg.add("JuMP")
 Pkg.add("SCS")
 Pkg.add("Plots")
+Pkg.add("PlotThemes")
 =#
 
 #= Conventions
@@ -15,6 +16,9 @@ using LinearAlgebra
 import Ipopt
 import Plots
 
+# Plot settings
+Plots.theme(:orange)
+
 # Create JuMP Model - nonlinear solver
 model = Model(optimizer_with_attributes(Ipopt.Optimizer))
 
@@ -22,20 +26,17 @@ model = Model(optimizer_with_attributes(Ipopt.Optimizer))
 # Constant Parameters
 const ge = [-9.807,0,0] # Earth's gravity
 const gm = [-3.7114,0,0] # Mars' gravity
-const maxThrust = 3100 # Maximum thrust (N)
+const maxThrust = 500 # Maximum thrust (N)
 const nThrusters = 6 # Number of thrusters 
 const ϕ = 27 # Cant angle of thrusters
 const I_sp = 225 # Specific Impulse (s)
 const m_wet = 1905 # Wet mass of spacecraft (kg)
 const m_dry = 1505 # Dry mass of spacecraft (kg)
-const α = 1/(I_sp*abs(ge[3])*cosd(ϕ)) # Constant for mass loss rate
-const minGlide = deg2rad(86) # Minimum glide angle / Cone angle "90 - minGlide"
-
-# SOCP Problem Formulation Stuff
-const S = [0 1 0 0 0 0; 0 0 1 0 0 0]
+const α = 1/(I_sp*abs(gm[1])*cosd(ϕ)) # Constant for mass loss rate
+const minGlide = deg2rad(4) # Minimum glide angle / Cone angle "90 - minGlide"
 
 # Time discretisation
-const n = 1000 # Number of points (mesh size)
+const n = 100 # Number of points (mesh size)
 const tf = 72 # Time of flight (s)
 const Δt = tf / n # Time step
 
@@ -48,80 +49,98 @@ const initialVy = 0 # Initial velocity (m/s)
 const initialVz = 100 # Initial velocity (m/s)
 
 # Variable constraints 
-const rho1 = 0.3 * maxThrust * nThrusters 
-const rho2 = 0.8 * maxThrust * nThrusters
+const rho1 = 0.3 * maxThrust * nThrusters * cosd(ϕ)
+const rho2 = 0.8 * maxThrust * nThrusters * cosd(ϕ)
 
-# Which variables do I need? Whcih variables need constraining?
-# Mass, Thrust, Displacement (relative to target), Velocity
+# Define variables
 @variables(model, begin
-    0 <= mass[1:n]  # Mass vector
-    displacement[1:n, 1:3] # 3Dof Displacement (relative to target) vector
-    velocity[1:n, 1:3]  # 3Dof Velocity vector
-    thrust[1:n, 1:3] # 3Dof Thrust vector
-    θ[1:n] <= minGlide # Glide angle
+    # Decision Variables
+    ux[1:n]
+    uy[1:n]
+    uz[1:n]
+    σ[1:n]
+    # Other variables
+    rx[1:n]
+    ry[1:n]
+    rz[1:n]
+    vx[1:n]
+    vy[1:n]
+    vz[1:n]
+    z[1:n]
+    z0[1:n]
 end)
 
-# Fix initial conditions
-# Inital mass = wet mass
-fix(mass[1], m_wet; force = true)
+# Boundary Conditions
+fix(z[1], log(m_wet); force = true)
+fix(z0[1], log(m_wet); force = true)
 
-# Inital displacement coordinates
-fix(displacement[1,1], initialX; force = true) 
-fix(displacement[1,2], initialY; force = true)
-fix(displacement[1,3], initialZ; force = true)
+fix(rx[1], initialX; force = true) 
+fix(ry[1], initialY; force = true)
+fix(rz[1], initialZ; force = true)
 
-# Intial velocity
-fix(velocity[1,1], initialVx; force = true)
-fix(velocity[1,2], initialVy; force = true)
-fix(velocity[1,3], initialVz; force = true)
+fix(vx[1], initialVx; force = true)
+fix(vy[1], initialVy; force = true)
+fix(vz[1], initialVz; force = true)
 
-# Fix final conditions
-# Final displacement coordinates
-fix(displacement[n,1], 0; force = true)
-fix(displacement[n,2], 0; force = true)
-fix(displacement[n,3], 0; force = true)
+#=
+fix(rx[n], 0; force = true)
+fix(ry[n], 0; force = true)
+fix(rz[n], 0; force = true)
 
-# Final velocity
-fix(velocity[n,1], 0; force = true)
-fix(velocity[n,2], 0; force = true)
-fix(velocity[n,3], 0; force = true)
+fix(vx[n], 0; force = true)
+fix(vy[n], 0; force = true)
+fix(vz[n], 0; force = true)
+=#
 
 # Translational dynamics model
-@expression(model, δm[j = 1:n], -α * (thrust[j,1]^2 + thrust[j,2]^2 + thrust[j,3]^2)^0.5)
-@expression(model, acceleration[j = 1:n, 1:3], thrust[j,1:3]./mass[j] .+ gm)
-
-# SOCP Constraints
-@expression(model, stateVector[1:6, j = 1:n], vcat(displacement[j,1:3], velocity[j,1:3]))
-@expression(model, c[1:6, j = 1:n], [-tan(minGlide); 0; 0; 0; 0; 0])
-@expression(model, SsV[1:2, j = 1:n], S*stateVector[1:6, j])
-# @expression(model, ph[1:2, j = 1:n], S*stateVector[j, 1:6])
-
-# Rotational dynamics model soon????
+@expression(model, μ1[j=1:n], rho1*exp(-z0[j]))
+@expression(model, μ2[j=1:n], rho2*exp(-z0[j]))
 
 # NOTE: Only use constraints for variables defined above, otherwise use expression for secondary variables
 for j = 2:n
     i = j-1
     @constraints(model, begin 
-        # Dynamics model constraints
-        mass[j] == mass[j-1] + Δt * δm[j-1]
-        velocity[j, 1:3] == velocity[i, 1] .+ (Δt .* acceleration[i,1:3])
-        displacement[j, 1:3] == displacement[i, 1:3] .+ (Δt .* velocity[i,1:3])
-        # Actual constraints
-        rho1 ≤ (thrust[j,1]^2 + thrust[j,2]^2 + thrust[j,3]^2)^0.5 ≤ rho2
-        displacement[j,1] ≥ 0
-        # θ[j] == atan(sqrt(displacement[j,1]^2+displacement[j,2]^2)/displacement[j,3]^2)
-        # SOCP constraint here 
-        # [-c[1:6, j]*stateVector[j, 1:6]; S*stateVector[j, 1:6]] in SecondOrderCone()
-        (SsV[1,j]^2+SsV[2,j]^2)^0.5 + (c[1:6,j]')*stateVector[1:6,j] ≤ 0
+    # fcn2optimexpr
+        z0[j] == log(m_wet - α * rho2 * Δt * i)
+        z[j] == z[i] - (-α * (σ[j] + σ[i])/2) * Δt/2
+        rx[j] == rx[i] + (vx[j] + vx[i]) * Δt/2 + (ux[j] + ux[i]) * Δt^2/12
+        ry[j] == ry[i] + (vy[j] + vy[i]) * Δt/2 + (uy[j] + uy[i]) * Δt^2/12
+        rz[j] == rz[i] + (vz[j] + vz[i]) * Δt/2 + (uz[j] + uz[i]) * Δt^2/12
+        vx[j] == vx[i] + ((ux[j] + ux[i])/2 + gm[1]) * Δt/2
+        vy[j] == vy[i] + ((uy[j] + uy[i])/2 + gm[2]) * Δt/2
+        vz[j] == vz[i] + ((uz[j] + uz[i])/2 + gm[3]) * Δt/2
+
+    # Actual Constraints
+        rx[j] ≥ 0
+        μ1[j]*(1 - (z[j]-z0[j]) + (z[j]-z0[j])^2/2) ≤ σ[j]
+        σ[j] ≤ μ2[j]*(1 - (z[i]-z0[i]))
+        z0[j] ≤ z[j]
+        z[j] ≤ log(m_wet - α*rho1*Δt*i)
     end)
 end
 
+for k = 1:n
+    @constraints(model, begin
+    0 ≤ σ[k]^2 - (ux[k]^2 + uy[k]^2 + uz[k]^2)
+    rho1^2 ≤ (ux[k]^2 + uy[k]^2 + uz[k]^2) * exp(z[k])^2
+    (ux[k]^2 + uy[k]^2 + uz[k]^2) * exp(z[k])^2 ≤ rho2^2
+    (ry[k]^2 + rz[k]^2) ≤ (rx[k]*tan(minGlide))^2
+    end)
+end
+
+# PROBLEM: IPOPT DOES NOT LIKE L2-NORMS
+# SOLUTION: SQUARE BOTH SIDES OF THE CONSTRAINTS
+
+# PROBLEM: Dynamics (@ j = 2) do not make physical sense
+# SOLUTION: ???
+
 # Set Objective: Minimize fuel usage/Maximise final mass
-@objective(model, Max, mass[n])
+@objective(model, Min, -z[n])
 
 # Run solver
 optimize!(model)
 solution_summary(model)
+
 
 
 # FUNCTIONS
@@ -129,7 +148,7 @@ function plotter(y; kwargs...)
     """
     Quick Plotter
     """
-        return plot(
+        return Plots.plot(
             (1:n) * Δt,
             value.(y);
             xlabel = "Time (s)",
@@ -138,9 +157,18 @@ function plotter(y; kwargs...)
         )
     end
 
+
 # Plot results 
-# Postion, Velocity, Acceleration, Net Force, Throttle Level, θ
-plotter(displacement[:,3], label = "Altitude", linewidth = 2)
-plotter(velocity[:,3], label = "Z-Velocity", linewidth = 2)
-plotter(acceleration[:,3], label = "Z-Acceleration", linewidth = 2)
-plotter(thrust[:,3], label = "Z-Thrust", linewidth = 2)
+x1 = plotter(rx[:], ylabel = "X-Disp", linewidth = 2)
+x2 = plotter(ry[:], ylabel = "Y-Disp", linewidth = 2)
+x3 = plotter(rz[:], ylabel = "Z-Disp", linewidth = 2)
+v1 = plotter(vx[:], ylabel = "X-Velocity", linewidth = 2)
+v2 = plotter(vy[:], ylabel = "Y-Velocity", linewidth = 2)
+v3 = plotter(vz[:], ylabel = "Z-Velocity", linewidth = 2)
+u1 = plotter(ux[:], ylabel = "X-Acceleration", linewidth = 2)
+u2 = plotter(uy[:], ylabel = "Y`-Acceleration", linewidth = 2)
+u3 = plotter(uz[:], ylabel = "Z-Acceleration", linewidth = 2)
+Plots.plot(x1, x2, x3, 
+        v1, v2, v3, 
+        u1, u2, u3,
+        layout = (3,3), legend = false)
